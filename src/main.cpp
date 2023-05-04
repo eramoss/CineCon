268ed3a8d4f0239f51508d723057dcffbd3b650d
eramoss <edulramos@outlook.com>
2023-05-04 15:43:28 -0300

    criando struct para passar parametros de data

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,19 @@
 
 int main(int argc, char const *argv[])
 {
-  Filme * fl = new Filme();
-  fl->setData({15,30,6,5,2023});
-  const time_t t = fl->getData();
-  fl->setNome("Wall-e");
+  Filme * filme = new Filme();
+  struct DataFilmeParams data = {
+    /*hora: */ 15,
+    /*minuto: */ 30,
+    /*dia: */ 6,
+    /*mês: */ 5,
+    /*ano: */ 2023,
+  };
+  
+  filme->setData(data);
+  const time_t t = filme->getData();
+  filme->setNome("Wall-e");
 
-  std::cout << "o filme " << fl->getNome() << " vai passar: "  << asctime(localtime(&t));
+  std::cout << "o filme " << filme->getNome() << " vai passar: "  << asctime(localtime(&t));
   return 0;
 }
