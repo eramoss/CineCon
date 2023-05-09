90886a36fdd1356b742aa512e0d2366fbbfd082f
eramoss <edulramos@outlook.com>
2023-05-09 13:48:12 -0300

    testing addSessao with FIlmes with conflict dates

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,21 @@ int main(int argc, char const *argv[])
 {
   Filme * filme = new Filme();
   struct DataFilmeParams data = {
+    /*hora: */ 16,
+    /*minuto: */ 30,
+    /*dia: */ 6,
+    /*mês: */ 5,
+    /*ano: */ 2023,
+  };
+
+  filme->setData(data);
+  filme->setNome("Wall-e");
+  filme->setDuracao(HOUR_IN_TIME_T);
+
+
+
+  Filme * filme1 = new Filme();
+  struct DataFilmeParams data1 = {
     /*hora: */ 15,
     /*minuto: */ 30,
     /*dia: */ 6,
@@ -13,41 +28,37 @@ int main(int argc, char const *argv[])
     /*ano: */ 2023,
   };
   
-  filme->setData(data);
-  filme->setNome("Wall-e");
-  Filme * filme1 = new Filme();
-  struct DataFilmeParams data1 = {
-    /*hora: */ 16,
-    /*minuto: */ 30,
-    /*dia: */ 6,
-    /*mês: */ 5,
-    /*ano: */ 2023,
-  };
-  
   filme1->setData(data1);
-  filme1->setNome("Wall-e");
-    Filme * filme2 = new Filme();
+  filme1->setNome("Inception");
+  filme1->setDuracao(MINUTE_IN_TIME_T);
+
+  Filme * filme2 = new Filme();
   struct DataFilmeParams data2 = {
-    /*hora: */ 17,
-    /*minuto: */ 30,
+    /*hora: */ 15,
+    /*minuto: */ 32,
     /*dia: */ 6,
     /*mês: */ 5,
     /*ano: */ 2023,
   };
   
-  filme1->setData(data2);
-  filme1->setNome("Wall-e");
+  filme2->setData(data2);
+  filme2->setNome("The grandFather");
+  filme2->setDuracao(HOUR_IN_TIME_T);
+ 
 
 
   Sala * sala = new Sala("a1", 12);
   Sala * sala1 = new Sala("a2", 12);
+
   Sessao * sessao = new Sessao(*filme,*sala);
   Sessao * sessao1 = new Sessao(*filme1,*sala);
-  Sessao * sessao2 = new Sessao(*filme, *sala1);
+  Sessao * sessao2 = new Sessao(*filme2,*sala);
 
   ListaSessao * ls = new ListaSessao();
   ls->addSessao(*sessao);
   ls->addSessao(*sessao1);
   ls->addSessao(*sessao2);
+
+
   return 0;
 }
