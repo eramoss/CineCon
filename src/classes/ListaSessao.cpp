0a6f44a7f4ff05381dd0c75a046a7066f82eb4b9
eramoss <edulramos@outlook.com>
2023-05-09 12:23:45 -0300

    add exception to addSessao

--- src/classes/ListaSessao.cpp
+++ src/classes/ListaSessao.cpp
@@ -7,16 +7,27 @@ ListaSessao::ListaSessao() : sessoes() {}
 ListaSessao::~ListaSessao() {}
 
 void ListaSessao::addSessao(const Sessao& sessao) {
+  
   if(sessoes.size() == 0) {
     sessoes.push_back(sessao);
     return;
   }
   for ( auto it = sessoes.begin(); it != sessoes.end(); ++it){
+    time_t INICIO_SESSAO = sessao.getFilme().getData();
+    time_t TERMINO_SESSAO = sessao.getFilme().getData() + sessao.getFilme().getDuracao();
+    time_t DATA_ITERATOR = it->getFilme().getData();
+
     if(sessao.getFilme().getData() == it->getFilme().getData() && sessao.getSala().getId() == it->getSala().getId()) {
         std::__throw_invalid_argument("ERROR: na função \"addSessao\", sessão tem mesmo horário e está na mesma sala de outra");  
         return; 
     }
+    if(sessao.getSala().getId() == it->getSala().getId()){
+      if(TERMINO_SESSAO > DATA_ITERATOR && DATA_ITERATOR > INICIO_SESSAO ) {
+             std::__throw_invalid_argument("ERROR: na função \"addSessao\", sessão já ocorrendo nesta sala");
+          }
+      }
   }
+  
   sessoes.push_back(sessao);
 }
 
