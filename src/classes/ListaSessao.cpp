cf430e41a170583c098befe975281ddf2fe3d7b4
eramoss <edulramos@outlook.com>
2023-05-09 13:47:19 -0300

    adjust exception treatment

--- src/classes/ListaSessao.cpp
+++ src/classes/ListaSessao.cpp
@@ -1,6 +1,7 @@
 #include "../headers/ListaSessao.h"
 #include <algorithm>
 #include <iostream>
+#include <string>
 
 ListaSessao::ListaSessao() : sessoes() {}
 
@@ -15,16 +16,22 @@ void ListaSessao::addSessao(const Sessao& sessao) {
   for ( auto it = sessoes.begin(); it != sessoes.end(); ++it){
     time_t INICIO_SESSAO = sessao.getFilme().getData();
     time_t TERMINO_SESSAO = sessao.getFilme().getData() + sessao.getFilme().getDuracao();
-    time_t DATA_ITERATOR = it->getFilme().getData();
+    time_t INICIO_ITERATOR = it->getFilme().getData();
+    time_t TERMINO_ITERATOR = it->getFilme().getData() + it->getFilme().getDuracao();
 
     if(sessao.getFilme().getData() == it->getFilme().getData() && sessao.getSala().getId() == it->getSala().getId()) {
         std::__throw_invalid_argument("ERROR: na função \"addSessao\", sessão tem mesmo horário e está na mesma sala de outra");  
         return; 
     }
     if(sessao.getSala().getId() == it->getSala().getId()){
-      if(TERMINO_SESSAO > DATA_ITERATOR && DATA_ITERATOR > INICIO_SESSAO ) {
-             std::__throw_invalid_argument("ERROR: na função \"addSessao\", sessão já ocorrendo nesta sala");
+      if(TERMINO_SESSAO >= INICIO_ITERATOR && INICIO_ITERATOR >= INICIO_SESSAO) {
+            std::string error_message = "Filme: "+ it->getFilme().getNome() + " tem inicio às: " +  std::to_string(localtime(&INICIO_ITERATOR)->tm_hour)  + ":" + std::to_string(localtime(&INICIO_ITERATOR)->tm_min) + ". E o filme inserido: " + sessao.getFilme().getNome() + " termina: " + std::to_string(localtime(&TERMINO_SESSAO)->tm_hour) + ":" + std::to_string(localtime(&TERMINO_SESSAO)->tm_min);             
+            std::__throw_invalid_argument(error_message.c_str());
           }
+        if(TERMINO_ITERATOR >= INICIO_SESSAO && INICIO_SESSAO >= INICIO_ITERATOR){
+            std::string error_message = "Filme: "+ it->getFilme().getNome() + " termina às: " +  std::to_string(localtime(&TERMINO_ITERATOR)->tm_hour)  + ":" + std::to_string(localtime(&TERMINO_ITERATOR)->tm_min) + ". E o filme inserido: " + sessao.getFilme().getNome() + " começa: " + std::to_string(localtime(&INICIO_SESSAO)->tm_hour) + ":" + std::to_string(localtime(&INICIO_SESSAO)->tm_min);             
+            std::__throw_invalid_argument(error_message.c_str());
+        }
       }
   }
   
